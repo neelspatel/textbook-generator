333b299666481171bbd2920622a077d74ed62708
Nithin Tumma <ntumma@college.harvard.edu>
2013-05-04 14:29:55 -0400

    new order functions

--- order.py
+++ order.py
@@ -1,6 +1,20 @@
 import nltk
 import math
 
+# get unique elements of a list preserve order
+def unique (lst):
+  checked = []
+  for el in lst:
+    if el not in checked:
+      checked.append(el)
+   return checked
+
+# get the ordered list of the first time that each keyword appears
+def get_ordered_keywords (wiki_text, keywords):
+  wiki_words = wiki_text.split()
+  wiki_keywords = filter (lambda x: x in keywords, wiki_words)
+  return unique(wiki_keywords)
+
 # get summaribility score
 def get_difference(wiki_dist, cur_dist):
   dist = 0.0
